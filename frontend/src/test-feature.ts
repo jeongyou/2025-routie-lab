44301231ee20d9af9b06a611ec557b1be9722d63
Youjeong Jeong <wjddbwjd1205@naver.com>
2025-09-01 22:40:48 +0900

    배 (#10)

--- frontend/src/test-feature.ts
+++ frontend/src/test-feature.ts
@@ -4,3 +4,4 @@ console.log('hello release-please');
 console.log('hello release-please');
 console.log('hello release-please');
 console.log('hello release-please');
+console.log('hello release-please');
